3966fc1928a0c35866f06f2c654a9e593d53765a
Roman Izegov <izegovrg@yandex.ru>
2025-06-16 18:59:12 +0300

    added functions

--- db.py
+++ db.py
@@ -15,4 +15,43 @@ CREATE TABLE IF NOT EXISTS tasks (
 cur.execute(query_1)
 con.close()
 
+def add_task(description, date):
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    query_add = '''
+        INSERT INTO tasks(description, date, done) VALUES (?, ?, ?)'''
+    
+    cur.execute(query_add, (description, date, 0))
+    con.commit()
+    con.close()
 
+def get_all_tasks():
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute ('''
+        SELECT*
+        FROM tasks ;
+    ''')
+    results = cur.fetchall()
+    con.close()
+    for result in results:
+        print(result)
+
+
+def delete_task(task_id):
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute('''
+        DELETE FROM tasks WHERE id = ?
+''', (task_id,))
+    con.commit()
+    con.close()
+
+def mark_done(task_id):
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute('''
+        UPDATE tasks SET done = 1 WHERE id = ?
+    ''', (task_id,))
+    con.commit()
+    con.close()
