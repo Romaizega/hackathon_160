48589e99ef8835406da6b9d7665ef036895b1b6c
Roman Izegov <izegovrg@yandex.ru>
2025-06-17 08:23:21 +0300

    some upgrade

---
+++ db.py
@@ -0,0 +1,69 @@
+import sqlite3
+from datetime import datetime
+
+
+con = sqlite3.connect("db.sqlite")
+
+cur = con.cursor()
+
+query_1 = '''
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY,
+    description TEXT NOT NULL,
+    date INTEGER,
+    done BOOLEAN NOT NULL DEFAULT 0
+);
+'''
+cur.execute(query_1)
+con.close()
+
+def add_task(description):
+    today = datetime.today().strftime('%Y%m%d')
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    query_add = '''
+        INSERT INTO tasks(description, date, done) VALUES (?, ?, ?)'''
+    
+    cur.execute(query_add, (description, today, 0))
+    con.commit()
+    con.close()
+
+def get_all_tasks():
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute ('''
+        SELECT*
+        FROM tasks ;
+    ''')
+    results = cur.fetchall()
+    con.close()
+    for result in results:
+        print(result)
+
+
+def delete_task(task_id):
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute('''
+        DELETE FROM tasks WHERE id = ?
+''', (task_id,))
+    con.commit()
+    con.close()
+
+def mark_done(task_id):
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute('''
+        UPDATE tasks SET done = 1 WHERE id = ?
+    ''', (task_id,))
+    con.commit()
+    con.close()
+
+def delete_all():
+    # USE only for tests
+    con = sqlite3.connect("db.sqlite")
+    cur = con.cursor()
+    cur.execute('''
+        DROP TABLE tasks
+''')
+    con.commit()
---
+++ main.py
@@ -0,0 +1,39 @@
+from db import add_task, get_all_tasks, delete_task, mark_done
+
+
+def get_user_menu_choise():
+
+    print ("""====== TODO LIST ======
+        1. Add a task
+        2. Show all tasks
+        3. Mark the task as done
+        4. Delete the task
+        5. Exit
+    """)
+    while True:
+        try:
+            user_input = int(input("Please eneter your choice: "))
+            if user_input not in range(1, 6):
+                print("Wrong number, try again")
+                continue
+        except:
+            print("Write onle a number")
+            continue
+        if user_input == 1:
+            new_task = input("Write your new task: ")
+            add_task(new_task)
+            print(f"Yor  taks: {new_task} added")
+            get_all_tasks()
+        elif user_input == 2:
+            get_all_tasks()
+        elif user_input == 3:
+            task_id = int(input("Enter task id for mark: "))
+            mark_done(task_id)
+        elif user_input == 4:
+            delete_id = int(input("Write task id for delete task: "))
+            delete_task(delete_id)
+        elif user_input == 5:
+            print("Goodbye")
+            break
+        
+get_user_menu_choise()
